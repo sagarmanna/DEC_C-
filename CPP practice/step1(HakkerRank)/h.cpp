5a1ac104bbf7b7a3113439e5d5aea10a940e64bb
Sagar Manna <121675250+sagarmanna@users.noreply.github.com>
2025-01-19 11:22:10 +0530

    Add files via upload

---
+++ CPP practice/step1(HakkerRank)/h.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+ int main(){
+char ch;
+cout<<"Enter the char :";
+cin>>ch;
+
+if(ch>='a'&& ch<='z'){
+    cout<<"lowercase";
+}else{
+    cout<<"upercase";
+}
+
+ }
---
+++ CPP practice/step1(HakkerRank)/rev.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+
+vector<int> vec;
+
+vec.push_back(0);
+vec.push_back(1);
+cout<<vec.size();
+return 0;    
+    
+}
